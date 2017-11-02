361bcdf2cf9eb34ab155bf45bb94132cfd636b60
Malind Kowalski <malind.nikulski@gmail.com>
2017-11-02 17:06:22 -0600

    Update 6kyu_FindDivisors.js

--- 6kyu_FindDivisors.js
+++ 6kyu_FindDivisors.js
@@ -1,15 +1,16 @@
 function divisors(integer) {
   const divisors = []
-  //create an array to hold the divisors
 
   for (let i = 2; i <= integer/2; i++) {
-    //start at 2 because every # is divisible by 1 and itself
     if (integer % i === 0) {
       divisors.push(i)
-      //every # that is divisible up to the main number will be pushed into the divisors array
     }
   }
   return divisors.length ? divisors : integer + ' is prime'
-  //if divisors length is true then return array of divisors
-  //else return # is prime
-};
+}
+
+// L2: create an array to hold the divisors
+// L4: start at 2 because every # is divisible by 1 and itself
+// L6: every # that is divisible up to the tested integer will be pushed into the divisors array
+// L9: if divisors length is true then return array of divisors
+// L9: else return # is prime
