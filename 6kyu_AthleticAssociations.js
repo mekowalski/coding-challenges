8155dfcb4dd0d32ecd9fdb94f68203c825d4cf22
Malind Kowalski <malind.nikulski@gmail.com>
2017-11-08 19:42:19 -0700

    added explanations for kata

--- 6kyu_AthleticAssociations.js
+++ 6kyu_AthleticAssociations.js
@@ -72,3 +72,43 @@ function secondsToTime(value) {
 
   return `${hours}|${minutes}|${seconds}`
 }
+
+//Thinking prior to writing(also wishing i saved all my progress since this took so long)
+//separate range, average and median into individual functions first then implement later
+//thinking of how to write the ending statement, concatenate or interpolate?
+//how do i remove the | and white space from the test array?
+//before, i thought to calculate against each hour, minute and second
+//BUT in doing so, it was actually incorrect, i thought the test were wrong but my thinking was wrong
+//to get straight forward calculated times i would need a base to calculate therefore convert the
+//time all into an individual of seconds, much easier to do the Math
+
+// L2-7: conditional for the random tests, to weed out the empty strings & return '' since tests wants
+// L3-4: return of ''
+// L5-6: so, if the tested string is empty nothing, empty then return '' else return the called function
+// L9: separate function to run the calculations against tested data/strings
+// L10: create variable 'array' to remove ',' from the data
+// L11: create variable 'eachIndividual' = to an empty [] to store the data that will soon be looped over
+// L13-15: for loop for the tested data to remove whitespace from both sides of string & remove |
+// L14: take the newly separated data and store in the 'eachIndividual' array for now
+// L19: 'rangeSeconds' holds numerical value from data, starts at 0 because nothing is yet calculated
+// L20: create variable 'playerSeconds' to store the seconds of each players time
+// L22: variable sumSeconds will increment and store total of seconds after time conversions
+// L23: converting each time into seconds and adding it altogether
+// L24: take each new sets of seconds and store in 'playerSeconds' array
+// L27: variable 'rangeSeconds' will now equate to the value of MAX seconds - MIN seconds (range calc)
+// L29: variable 'rangeTime' to equate return value for the end statement
+// L33: 'total' will increment and store the final value of all playerSeconds times
+// L35: add all playerSeconds sets to what it totals until the playerSeconds length is reached
+// L38: 'averageSeconds' now equates to value of total seconds / the playerSeconds length (average calc)
+// L40: 'averageTime' is the return value for the end statement
+// L44: variable 'values' of playerSeconds sorted
+// L45: calculate what 'half' of values length will be
+// L46: 'medianValue' holds the numerical value for the median of the data
+// L49: if values length is odd then return the value in the middle
+// L51: else return the value of (low middle + high middle) / 2
+// L53: 'medianTime' is the return value for the end statement
+// L57-59: each variable stores the calculated & converted times of their perspective value
+// L61: returns the ending statement formatted to specific requirement of tests
+// L65-67: converts seconds back to respective times(hours, minutes, seconds)
+// L69-71: conditionals for if conversions return single digit values, tests formats with double digits
+// L73: return each of those variables in formatted statement
