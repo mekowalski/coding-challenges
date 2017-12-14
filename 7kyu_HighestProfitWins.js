4feb437c31bb41c6da099d7d2ebeb3067a132b15
Malind Kowalski <malind.nikulski@gmail.com>
2017-12-13 17:25:10 -0700

    codewars 7kyu kata passed

---
+++ 7kyu_HighestProfitWins.js
@@ -0,0 +1,6 @@
+function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)]
+}
+
+// L2: return the Math minimum and Math maximum of the array, use spread operator for including both
+// L2: integers in a combined array
