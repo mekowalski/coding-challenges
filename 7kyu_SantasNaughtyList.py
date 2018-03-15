9f6bb1cd04122410988a012a699ab3d63d1a5878
Malind Kowalski <malind.nikulski@gmail.com>
2018-03-15 10:36:51 -0600

    add description to kata

--- 7kyu_SantasNaughtyList.py
+++ 7kyu_SantasNaughtyList.py
@@ -6,3 +6,14 @@ def find_children(List, children):
       good_kids.append(child)
 
   return sorted(good_kids)
+
+# tried this one with JavaScript with a correct solution but it didn't pass the random tests only
+# the sample tests provided
+# purpose of this kata is to return chilrens' names that were in the arrays of both List and
+# children without duplication
+
+# L2: create new arry to hold list of childrens' names matching both arrays
+# L4: loop child in children array
+# L5: if that same child in children array is in List array and not in the new good_kids array then
+# L6: add that child to the good_kids array
+# L8: and return the good_kids array sorted aphabetically
