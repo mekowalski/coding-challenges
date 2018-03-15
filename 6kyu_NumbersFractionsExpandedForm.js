67eed11fd11742fabe9e7da727aebf05bbade786
Malind Kowalski <malind.nikulski@gmail.com>
2018-03-15 15:13:54 -0600

    modify kata code

--- 6kyu_NumbersFractionsExpandedForm.js
+++ 6kyu_NumbersFractionsExpandedForm.js
@@ -1,41 +1,19 @@
 function expandedForm(num) {
-// copied code from part 1
-  let numberArray = num.toString().split('').reverse();
+  let numberArray = num.toString().split('')
   let expandedForm = ''
 
-  for (let i = 0; i < numberArray.length; i++) {
+  for (let i = numberArray.length - 1, pow = i; i >= 0; i--) {
     let currentNum = numberArray[i]
-    console.log(currentNum)
-    if (currentNum != 0)
-      expandedForm += `${currentNum * Math.pow(10, i)} + `
+
+    if (currentNum !== '.')
+      pow--
+
+    if (currentNum !== '0' && parseInt(currentNum))
+      expandedForm += `${currentNum}/${Math.pow(10, pow)} + `
   }
   expandedForm = expandedForm.split(' ').reverse().join(' ')
-  return expandedForm.replace(' + ','')
-
-
-// console.log(toString.call(num)) => prints the object type on screen
-  let numberArray = num.toString().split('')
-  let decimal = numberArray.indexOf('.')
-  let newNumberArray = numberArray.splice(`${decimal + 1}`, `${numberArray.length}`)
-
-  let firstDigit = Math.floor(num)
-  // need to expand all numbers before decimal and expand all numbers after decimal in fraction form
-  // 3.75
-  // 3
-  // 36.75
-  // 36
-  // 30 + 6 + 7/100 + 5/10
-
-  let expandedForm = ''
-
-  for (let i = 0; i < newNumberArray.length; i++) {
-    let currentNum = newNumberArray[i]
-
-    if (currentNum != 0)
-     expandedForm += `${currentNum}/` + `${10 * Math.pow(10, i)} + `
-  }
-  let final = (`${firstDigit} + ` + `${expandedForm}`).split(' ').join(' ')
-  return final.substring(0, final.length - 3)
-
-// finalExpandedForm = expandedForm.split(' ').reverse().join(' ')
+  return expandedForm.replace(' + ','').replace(/\/\d+/,'')
 }
+
+//this is a 3rd attempt, i forgot to copy over the 2nd attempt with updates
+//this code only passes sample tests not the random attempted tests, not passing yet
