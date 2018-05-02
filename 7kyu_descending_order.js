1eb010e0afab5cc2167c0c15736fc0702c2bacc9
Malind Kowalski <malind.nikulski@gmail.com>
2018-05-02 14:09:15 -0600

    working on descending order kata

---
+++ 7kyu_descending_order.js
@@ -0,0 +1,23 @@
+function descendingOrder(n){
+  let num = n.toString().split()
+  let sortArray = []
+
+  for (let i = 0; i < num.length; i++) {
+    //num.sort(function(a, b) {return a-b})
+    sortArray.push(i)
+  }
+
+  console.log(num)
+
+
+  //if (num.length > 1) {
+    //let sortArray = num.sort(function(a, b) {
+      //return a - b
+    //})
+  //}
+  //let joinArray = sortArray.join('')
+ // let sortNum = joinArray.parseInt('')
+  //return sortNum
+}
+
+//what i have so far. working with hilary
