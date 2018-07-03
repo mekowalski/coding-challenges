1e155ac8ada40e649102ba4ee204b883d9db3df9
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-03 13:29:27 -0600

    beginning attempt for leetcode medium ThreeSum problem

---
+++ lc_med_ThreeSum.js
@@ -0,0 +1,13 @@
+//@param {number[]} nums
+//@return {number[][]}
+//Leetcode
+
+//Beginning attempt
+var threeSum = function(nums) {
+    const array = []
+    for (let i = 0; i < nums.length; i++) {
+        const currentNumber = nums[i]
+        const diffOne = currentNumber
+        const diffTwo = diffOne
+    }
+};
